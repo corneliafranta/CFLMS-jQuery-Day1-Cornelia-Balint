906b91707505abbebeffdfa9a91f0e3b7fb7c8e6
Balint Bakos <balint.bb@gmail.com>
2020-06-09 10:02:14 +0200

    adds ex1

---
+++ ex1-balint/myscript.js
@@ -0,0 +1,45 @@
+/*
+ * My jQuery selectors
+ *
+ * Apply a style such as a yellow background to the following elements.
+ * Test your selectors one at a time.
+ */
+
+// all elements
+// $("*").css("background-color", "yellow");
+
+// all p elements
+// $('p').css("background-color", "yellow")
+
+// the p element with an ID of "intro"
+// $('#intro').css('background-color', 'yellow')
+
+// all elements with a class of "note"
+// $('.note').css('background-color', 'yellow')
+
+// all DIV elements with a class of note
+// $('div.note').css('background-color', 'yellow')
+
+// all p elements in the article element
+// $('article p').css('background-color', 'yellow')
+
+
+// the last element in any section which is a p tag
+// $('section p:last-child').css('background-color', 'yellow')
+
+
+// the first paragraph on the page
+// $("p:first").css('background-color', 'yellow')
+
+
+// all p elements which have a link	
+// $("p > a").css('background-color', 'yellow')
+
+
+// the second li within nested ul element
+// $("ul li li:eq(1)").css('background-color', 'yellow')
+
+
+// every second and third p element
+// $("p:nth-child(2)").css('background-color', 'yellow')
+// $("p:nth-child(3)").css('background-color', 'yellow')
